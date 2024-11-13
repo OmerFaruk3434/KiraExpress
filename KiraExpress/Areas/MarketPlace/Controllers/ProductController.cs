5d12a28f750074dbd00dc42bebba052f62311d40
Ömer Faruk Baysal <omerfarukbaysal3434@gmail.com>
2024-11-13 17:50:41 +0300

    Updated

---
+++ KiraExpress/Areas/MarketPlace/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KiraExpress.Areas.MarketPlace.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
